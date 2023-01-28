745b06fb6a00d599da21d6073f570eb03bbad8d1
Summer S <snowykittin@gmail.com>
2023-01-28 00:50:29 -0500

    initial push

---
+++ app/app.js
@@ -0,0 +1,59 @@
+//FUNKY FRESH BALL TEXTURES
+function addBalls(){
+// Some random colors
+const colors = ["#D6CFCB", "#565264", "#A6808C", "#000", "#706677"];
+
+const numBalls = 100;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.right = `${Math.floor(Math.random() * 85)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 85)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  //grab "welcome"
+  let intro = document.getElementById("intro")
+  intro.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
+}
+
+function welcomeType(){
+   //run welcome type first
+    $("#heading").fadeIn(1000);
+   //then load in the "about"
+    $("#about").fadeIn(1500);
+}
+
+
+$(document).ready(function(){
+    welcomeType();
+    addBalls();
+});
